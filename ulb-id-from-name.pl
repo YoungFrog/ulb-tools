b5d8c829ec5c7b0fe54efa88f07ecb4ce30a87a7
Nicolas Richard <youngfrog@members.fsf.org>
2016-05-10 16:50:08 +0200

    new file

---
+++ ulb-id-from-name.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+use strict;
+use WWW::Mechanize;
+use HTML::TreeBuilder;
+binmode STDOUT, ":utf8"; # spit utf8 to terminal
+use utf8; # allow for utf8 inside the code.
+
+my $name = shift;
+
+die "Usage: $0 search-string\n" unless $name;
+my $m = WWW::Mechanize->new();
+my $tree = HTML::TreeBuilder->new;
+
+# récupération du formulaire
+$m->get('http://www.ulb.ac.be/commons/phonebook?mode=id-search');
+
+die 'Échec de connexion : ' . $m->res->status_line()
+  unless $m->success();
+
+# remplissage et validation du formulaire
+$m->submit_form(
+                form_number => 1,
+                fields => {
+                           keyword => "$name"
+                          }
+               );
+
+
+# connexion réussie ?
+die 'Échec de validation du formulaire : ' . $m->res()->status_line()
+  unless $m->success();
+
+$tree->parse_content($m->content());
+
+my @results = $tree->look_down("_tag","div","class","phonebookEntry");
+
+die "No result found.\n" unless @results;
+
+sub show_result {
+  my $tree = $_;
+  my $name = $tree->look_down("_tag","div","class",'phonebookName')->as_text();
+  my $id = $tree->look_down("_tag","div","class",'phonebookId')->as_text();
+  if ($id) {
+    $id =~ s/^.*://;
+  }
+  else {
+    $id = "No id";
+  }
+  print "$id : $name\n"
+};
+
+map (show_result, @results);
